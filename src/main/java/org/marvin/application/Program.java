ba04cb8ff912447d6ab55a2e5dd33efd66ac0cd3
Marvinx9 <afraniodantas0224@gmail.com>
2025-06-04 13:21:13 -0300

    first commit

---
+++ src/main/java/org/marvin/application/Program.java
@@ -0,0 +1,72 @@
+package org.marvin.application;
+
+import org.marvin.entities.ImportedProduct;
+import org.marvin.entities.Product;
+import org.marvin.entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+        List<Product> products = new ArrayList<>();
+
+        for(int i=1; i<=n; i++){
+            System.out.println("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char typeProduct = sc.next().charAt(0);
+
+            if(typeProduct == 'i'){
+                System.out.print("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                double price = sc.nextDouble();
+                System.out.print("Customs fee: ");
+                double customsFee = sc.nextDouble();
+
+                ImportedProduct imp = new ImportedProduct(name, price, customsFee);
+                products.add(imp);
+            }
+            if(typeProduct == 'c'){
+                System.out.print("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                double price = sc.nextDouble();
+
+                Product p = new Product(name, price);
+                products.add(p);
+            }
+            if(typeProduct == 'u'){
+                System.out.print("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.print("Price: ");
+                double price = sc.nextDouble();
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
+                sc.nextLine();
+                Date date = formatter.parse(sc.nextLine());
+
+                UsedProduct up = new UsedProduct(name, price, date);
+                products.add(up);
+            }
+        }
+
+        System.out.println("PRICE TAGS:");
+        for(Product product : products){
+            System.out.println(product.priceTag());
+        }
+
+        sc.close();
+
+    }
+}
---
+++ src/main/java/org/marvin/entities/ImportedProduct.java
@@ -0,0 +1,32 @@
+package org.marvin.entities;
+
+public class ImportedProduct extends Product{
+    private Double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag(){
+        return super.getName()
+                + " $ "
+                + (super.getPrice() + customsFee)
+                + " (Customs fee: $ "
+                + customsFee
+                + ")";
+    }
+}
